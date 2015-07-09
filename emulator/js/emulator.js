a006980f7428d543d20d6abb69b8b081e1c79c1c
Rich Tibbett <rich.tibbett@gmail.com>
2015-07-09 07:59:57 +0200

    Split emulator utility functions out to a seperate JS file

--- emulator/js/emulator.js
+++ emulator/js/emulator.js
@@ -1,10 +1,3 @@
-function getParameterByName( name ) {
-	name = name.replace( /[\[]/, "\\[" ).replace( /[\]]/, "\\]" );
-	var regex = new RegExp( "[\\?&]" + name + "=([^&#]*)" ),
-		results = regex.exec( location.search );
-	return results === null ? "" : decodeURIComponent( results[ 1 ].replace( /\+/g, " " ) );
-}
-
 window.addEventListener( 'load', function() {
 
 	var selfUrl = new URL( window.location );
